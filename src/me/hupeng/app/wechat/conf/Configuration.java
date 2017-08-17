3dd2cde17b77e3a8d33b26546457c3dfd2c6ceee
hupeng <hupeng@imudges.com>
2017-08-17 18:36:57 +0800

    update

--- src/me/hupeng/app/wechat/conf/Configuration.java
+++ src/me/hupeng/app/wechat/conf/Configuration.java
@@ -4,7 +4,7 @@ public class Configuration {
     private static String HOST = "123.206.21.155";
 //    private static String HOST = "10.25.44.122";
     private static String PORT = "8080";
-    private static String APP = "WeatherWeb/v1";
+    private static String APP = "WechatWeb/v1";
 
     public static String BASE_HTTP_URL = "http://" + HOST + ":" + PORT +  (APP.equals("")?"/":"/" + APP + "/");
     public static String BASE_WS_URL = "ws://" + HOST + ":" + PORT + (APP.equals("")?"/":"/" + APP + "/") ;
